3cf0c5b930eb64775d092930dfae798888d71148
Murddy <thomas.lienard2@gmail.com>
2025-03-13 08:30:56 +0100

    init

---
+++ TP3-CAR/src/test/java/com/example/TP3_CAR/Tp3CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP3_CAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
